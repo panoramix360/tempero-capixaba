1b0e67be2412e0fd0784847220d0663954f2f48f
rennanc <rennanchagas@hotmail.com>
2016-05-09 22:57:10 -0300

    Adição de splash screen completo e funcional.

---
+++ TemperoCapixaba/app/src/main/java/com/creativityloop/android/temperocapixaba/activity/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.creativityloop.android.temperocapixaba.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.creativityloop.android.temperocapixaba.R;
+
+public class SplashActivity extends Activity {
+
+    /** tempo de duração **/
+    private final int SPLASH_DISPLAY_LENGTH = 3500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(getApplicationContext(),CardapioDiarioActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
